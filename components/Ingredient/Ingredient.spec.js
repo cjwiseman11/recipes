1d299b600e7d7c3cc624a71d26537a1ca1a58aa4
Chris <cjwiseman11@gmail.com>
2021-05-14 13:03:48 +0100

    Ingredient coverage

--- components/Ingredient/Ingredient.spec.js
+++ components/Ingredient/Ingredient.spec.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
 
 import Ingredient from './index';
 
 describe('<Ingredient />', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   test('Ingredient is rendered with a span by default', async () => {
     const { container } = render(<Ingredient ingredient="A tasty pie" />);
 
@@ -23,5 +28,23 @@ describe('<Ingredient />', () => {
     );
 
     expect(component.getByRole('listitem')).toHaveTextContent('This has some info!');
+  });
+
+  test('onMouseEnter and onMouseLeave triggers prop onHighlight function', () => {
+    const onHighlight = jest.fn();
+    const component = render(
+      <Ingredient ingredient="A tasty pie" onHighlight={onHighlight} Component="li" />
+    );
+    fireEvent.mouseOver(component.getByRole('listitem'));
+    expect(onHighlight).toHaveBeenNthCalledWith(1, 'A tasty pie');
+    fireEvent.mouseLeave(component.getByRole('listitem'));
+    expect(onHighlight).toHaveBeenNthCalledWith(2, null);
+  });
+
+  test('When component is highlighted, render with highlight class', () => {
+    const component = render(
+      <Ingredient ingredient="A tasty pie" Component="li" isHighlighted />
+    );
+    expect(component.getByRole('listitem')).toHaveClass('hover');
   });
 });
--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
   Thanks for the config https://github.com/vercel/next.js/blob/master/examples/with-jest/jest.config.js
 */
 module.exports = {
-  collectCoverageFrom: ['**/*.{js,jsx,ts,tsx}', '!**/*.d.ts', '!**/node_modules/**'],
+  collectCoverageFrom: ['**/*.{js,jsx,ts,tsx}', '!**/*.d.ts', '!**/node_modules/**', '!**/out/**', '!**/.next/**', '!**/coverage/**', '!jest.config.js'],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   transform: {
